74ecd6f0cf99dd1dddf9ae00d09dc5e3ba71c869
John Anthony Rivera <askinganthony@gmail.com>
2016-07-07 23:14:36 -0700

    Add sprite class

---
+++ src/components/Sprite.js
@@ -0,0 +1,19 @@
+let DRAW_TYPES = {
+  circle: function()
+}
+
+class Sprite {
+  constructor({type, position, color, radius=40}) {
+    switch type {
+      case 'circle':
+        this.draw = function(ctx) {
+          ctx.arc(this.position.x, this.position.y, this.radius, 0, 2*Math.PI);
+        }
+        break;
+    }
+    this.type = type;
+    this.position = position;
+    this.color = color;
+    this.radius = radius;
+  }
+}
--- src/slitherbots.js
+++ src/slitherbots.js
@@ -5,6 +5,18 @@ console.log(game);
 game.engine.initialize({canvas: canvas});
 game.engine.logCanvas();
 
+var slitherbot = new Sprite({
+  type: 'circle',
+  position: {x: 0, y: 0},
+  color: 'blue'
+});
+
+game.engine.render.addSprite(slitherbot);
+
+game.engine.render.update();
+
+
+
 /*
 var ctx = c.getContext("2d");
 ctx.fillStyle = "#FF0000";
